464afe2adaaa67b870f6e18fd613f0b9ac5f2e65
Yann Quintin <quintin.yann@gmail.com>
2020-12-10 23:01:41 +0100

    removing ../ before client in app.js

--- server/app.js
+++ server/app.js
@@ -87,7 +87,7 @@ if (process.env.NODE_ENV === 'production') {
 
 	// get the index.html that will be rendered on the browser
 	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname + '../client', 'build', 'index.html'));
+		res.sendFile(path.join(__dirname + 'client', 'build', 'index.html'));
 	});
 }
 
